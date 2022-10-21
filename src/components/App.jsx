ba4b47cf58457d1b55053bfcf85ec4f7a8939c2e
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-10-22 00:17:44 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import axios from 'axios';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    picture: [],
+  };
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://pixabay.com/api/?q=cat&page=1&key=29826556-a4f91074fca654992db1f732d&image_type=photo&orientation=horizontal&per_page=12'
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  render() {
+    return <div></div>;
+  }
+}
