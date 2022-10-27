7fb7911d616333a6ac5bff4f6da5452497379f27
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-10-27 13:57:36 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,41 +21,46 @@ export class App extends Component {
     this.setState(() => ({
       picture: [],
       inputText: data,
-      loading: true,
       page: 1,
       totalHits: null,
     }));
   };
   counterPage = () => {
-    this.setState(prevState => ({ page: (prevState.page += 1) }));
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    try {
-      if (
-        prevState.inputText !== this.state.inputText ||
-        prevState.page !== this.state.page
-      ) {
-        const response = await axios.get(
-          `https://pixabay.com/api/?q=${this.state.inputText}&page=${this.state.page}&key=29826556-a4f91074fca654992db1f732d&image_type=photo&orientation=horizontal&per_page=12`
-        );
-        const pictures = response.data.hits;
-        if (!pictures.length) {
-          alert('Упс, по вашому запиту картинки не знайдені');
-          this.setState(() => ({ loading: false }));
-        } else {
-          this.setState(prevState => ({
-            picture: [...prevState.picture, ...pictures],
-            totalHits: response.data.totalHits,
-            loading: false,
-          }));
+    if (
+      prevState.inputText !== this.state.inputText ||
+      prevState.page !== this.state.page
+    ) {
+      try {
+        if (
+          prevState.inputText !== this.state.inputText ||
+          prevState.page !== this.state.page
+        ) {
+          this.setState(() => ({ loading: true }));
+          const response = await axios.get(
+            `https://pixabay.com/api/?q=${this.state.inputText}&page=${this.state.page}&key=29826556-a4f91074fca654992db1f732d&image_type=photo&orientation=horizontal&per_page=12`
+          );
+          const pictures = response.data.hits;
+          if (!pictures.length) {
+            alert('Упс, по вашому запиту картинки не знайдені');
+          } else {
+            this.setState(prevState => ({
+              picture: [...prevState.picture, ...pictures],
+              totalHits: response.data.totalHits,
+            }));
+          }
         }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.setState(() => ({ loading: false }));
       }
-    } catch (error) {
-      console.log(error);
-      this.setState(() => ({ loading: false }));
     }
   }
+
   openModal = e => {
     const srcPicture = e.target.alt;
     this.setState(() => ({
